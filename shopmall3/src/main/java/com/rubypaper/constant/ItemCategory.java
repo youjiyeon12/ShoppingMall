ba60ecdce6a153e200bc8768fbf947be85034a55
HBAKM <star135058@naver.com>
2025-12-05 17:15:02 +0900

    재커밋

---
+++ shopmall3/src/main/java/com/rubypaper/constant/ItemCategory.java
@@ -0,0 +1,5 @@
+package com.rubypaper.constant;
+
+public enum ItemCategory {
+    WOMEN, MEN, KIDS
+}
--- shopmall3/src/main/java/com/rubypaper/constant/ItemSellState.java
+++
@@ -1,8 +0,0 @@
-package com.rubypaper.constant;
-
-public enum ItemSellState {
-   SELL, SOLD_OUT
-   //0    1    2   3
-}
-
-
